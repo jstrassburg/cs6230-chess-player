ec0af5c99190647af28eae15221dfcd9aa486831
JiM Strassburg <jstrassburg@gmail.com>
2021-10-26 16:54:14 -0500

    first shot at most of player and board state node classes

--- chess_player/BoardStateTreeNode.py
+++ chess_player/BoardStateTreeNode.py
@@ -1,21 +1,31 @@
 import chess
+from random import sample
 from Scorer import Scorer
+from statistics import mean
 
 
 class BoardStateTreeNode:
     def __init__(self, board: chess.Board, move: chess.Move = None, max_children: int = None):
-        self._board = board
-        self._move = move
+        self._board = board.copy(stack=False)
+        self.move = move
         self._max_children = max_children
         self._turn_color = chess.WHITE
         self._is_end_game = board.is_game_over()
-        self._children = list[BoardStateTreeNode]
+        self._children = None
 
     def populate_tree(self, depth: int):
-        pass
+        if depth == 0:
+            return
+
+        all_legal_moves = self._board.legal_moves
+        moves = all_legal_moves if self._max_children is None else sample(list(all_legal_moves), self._max_children)
+        self._children = [BoardStateTreeNode(self._board, move, self._max_children) for move in moves]
+
+        for child in self._children:
+            child.populate_tree(depth - 1)
 
     def enumerate_moves(self) -> list[chess.Move]:
-        pass
+        return [child.move for child in self._children]
 
-    def evaluate_moves(self, score_func: Scorer, agg_func) -> list[(float, chess.Move)]:
+    def evaluate_moves(self, score_func: Scorer, agg_func=mean) -> list[(float, chess.Move)]:
         pass
--- chess_player/Player.py
+++ chess_player/Player.py
@@ -1,60 +1,59 @@
 from abc import ABC, abstractmethod
 from BoardStateTreeNode import BoardStateTreeNode
 import chess
+import random
+from statistics import mean
 
 
 class Player(ABC):
     def __init__(self):
-        self._moves = list[chess.Move]
+        self._root = None
+
+    def play_move(self, board: chess.Board):
+        self._enumerate_game_states(board)
+        move = self._choose_move(board.turn)
+        board.push(move)
 
     @abstractmethod
     def _enumerate_game_states(self, board: chess.Board):
         pass
 
     @abstractmethod
-    def _score_moves(self):
-        pass
-
-    @abstractmethod
-    def _choose_move(self):
-        pass
-
-    @abstractmethod
-    def play_move(self, board: chess.Board):
+    def _choose_move(self, turn) -> chess.Move:
         pass
 
 
 class RandomPlayer(Player):
     def _enumerate_game_states(self, board: chess.Board):
-        pass
+        self._root = BoardStateTreeNode(board, move=None)
+        self._root.populate_tree(depth=1)
 
-    def _score_moves(self):
-        pass
-
-    def _choose_move(self):
-        pass
-
-    def play_move(self, board: chess.Board):
-        pass
+    def _choose_move(self, turn) -> chess.Move:
+        moves = self._root.enumerate_moves()
+        return random.choice(moves)
 
 
 class SearchPlayer(Player):
-    def __init__(self, search_depth, score_func, agg_func, max_children=None):
+    def __init__(self, search_depth, score_func, agg_func=mean, max_children=None):
         super().__init__()
         self._search_depth = search_depth
         self._score_func = score_func
         self._agg_func = agg_func
         self._max_children = max_children
-        self._root = BoardStateTreeNode()
 
     def _enumerate_game_states(self, board: chess.Board):
-        pass
+        self._root = BoardStateTreeNode(board, move=None, max_children=self._max_children)
+        self._root.populate_tree(depth=self._search_depth)
 
-    def _score_moves(self):
-        pass
-
-    def _choose_move(self):
-        pass
-
-    def play_move(self, board: chess.Board):
-        pass
+    def _choose_move(self, turn) -> chess.Move:
+        scored_moves = self._root.evaluate_moves(self._score_func, self._agg_func)
+        best_move = None
+        best_move_score = 0.0
+        for score, move in scored_moves:
+            if turn == chess.WHITE:
+                if score > best_move_score:
+                    best_move = move
+            else:
+                if score < best_move_score:
+                    best_move = move
+        return best_move
