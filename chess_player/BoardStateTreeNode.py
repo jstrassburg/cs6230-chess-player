2c9ec0138851c0a2651c879887a053e716ac3252
JiM Strassburg <jstrassburg@gmail.com>
2021-10-28 10:26:14 -0500

    impl for evaluate_moves and first test

--- chess_player/BoardStateTreeNode.py
+++ chess_player/BoardStateTreeNode.py
@@ -8,6 +8,8 @@ from statistics import mean
 class BoardStateTreeNode:
     def __init__(self, board: chess.Board, move: chess.Move = None, max_children: int = None):
         self._board = board.copy(stack=False)
+        if move is not None:
+            self._board.push(move)
         self.move = move
         self._max_children = max_children
         self._turn_color = chess.WHITE
@@ -28,5 +30,18 @@ class BoardStateTreeNode:
     def enumerate_moves(self) -> list[chess.Move]:
         return [child.move for child in self._children]
 
-    def evaluate_moves(self, score_func: Scorer, agg_func=mean) -> list[(float, chess.Move)]:
-        pass
+    def evaluate_moves(self, scorer: Scorer, agg_func=mean) -> list[(float, chess.Move)]:
+        # There are three cases here:
+        #   1. A leaf node should return it's board's score
+        #   2. A root node should return the list of all of its children's score's and moves
+        #   3. An internal node should return the aggregate (agg_func) of its children's scores
+        if not self._children:   # I am a leaf on the wind...~`
+            my_score = scorer.evaluate(self._board)
+            return [(my_score, self.move)]
+        elif self.move is None:  # I am [G]root.
+            return [child.evaluate_moves(scorer, agg_func) for child in self._children]
+        else:                    # It just takes some time. Little girl, you're in the middle of the ride(tree)
+            child_scores_and_moves = [child.evaluate_moves(scorer, agg_func) for child in self._children]
+            child_scores = list(zip(*child_scores_and_moves))[0]
+            child_scores_agg = agg_func(child_scores)
+            return [(child_scores_agg, self.move)]
--- chess_player_tests/BoardStateTreeNode_tests.py
+++ chess_player_tests/BoardStateTreeNode_tests.py
@@ -1,12 +1,14 @@
 import unittest
 import chess
 from chess_player.BoardStateTreeNode import BoardStateTreeNode
+from chess_player.Scorer import  SimplifiedEvaluationFunction
 
 
 class TestBoardStateTreeNode(unittest.TestCase):
     def setUp(self) -> None:
         self._board = chess.Board()
         self._node = BoardStateTreeNode(self._board)
+        self._scorer = SimplifiedEvaluationFunction()
 
     def test_populate_tree_no_depth(self):
         expected = 0
@@ -19,3 +21,13 @@ class TestBoardStateTreeNode(unittest.TestCase):
         self._node.populate_tree(depth=1)
         actual = len(self._node.enumerate_moves())
         self.assertEqual(expected, actual)
+
+    def test_evaluate_moves_leaf_node(self):
+        move = chess.Move.from_uci('e2e4')
+        # this should be a leaf node since I haven't called populate_tree
+        leaf_node = BoardStateTreeNode(self._board, move)
+        actual = leaf_node.evaluate_moves(self._scorer)
+        self.assertEqual(1, len(actual), 'Unexpected number of results returned.')
+        score, move = actual[0]
+        self.assertEqual(40, score, 'Unexpected score returned')
+        self.assertEqual(chess.Move.from_uci('e2e4'), move, 'Unexpected move returned.')
